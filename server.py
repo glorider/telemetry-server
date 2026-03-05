a89378be376c521fb0bff0589151bb0d1cc0f311
glorider <kernel0x01@gmail.com>
2026-03-05 12:09:42 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"status": "running"}
+
+@app.post("/collect")
+async def collect(request: Request):
+    data = await request.json()
+    print(data)
+    return {"status": "received"}
